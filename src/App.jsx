a9c14b3d4937ca46feba3f66d6ae8b86b192c4d3
dee797 <178827694+dee797@users.noreply.github.com>
2024-11-11 17:09:26 -0700

    Add logic for handling input changes and form submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
 import General from './components/General'
-import './App.css'
 import Education from './components/Education'
 import Experience from './components/PracticalExperience'
+import './App.css'
 
 function App() {
+  const [formData, setFormData] = useState();
+  const [educationItems, setEducationItems] = useState([{id: 1, key: crypto.randomUUID()}]);
+  const [experienceItems, setExperienceItems] = useState([{id: 1, key: crypto.randomUUID()}]);
+
+  const handleInputChange = event => {
+    const {name, value} = event.target;
+    setFormData({ ...formData, [name]: value});
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(formData);
+  }
 
   return (
     <>
@@ -12,10 +25,14 @@ function App() {
       <p>
         Fill out the info below to create your own resume
       </p>
-      <form>
-        <General />
-        <Education />
-        <Experience />
+      <hr/>
+      <form onSubmit={handleSubmit}>
+        <General inputChange={handleInputChange}/>
+        <hr/>
+        <Education inputChange={handleInputChange} educationItems={educationItems} setEducationItems={setEducationItems}/>
+        <hr/>
+        <Experience inputChange={handleInputChange} experienceItems={experienceItems} setExperienceItems={setExperienceItems}/>
+        <button type='submit'>Submit</button>
       </form>
     </>
   )
