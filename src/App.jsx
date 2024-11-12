f52c1c2d8e9d55932f77ceb775a5ede859b4a8c0
dee797 <178827694+dee797@users.noreply.github.com>
2024-11-12 15:32:24 -0700

    Functionality for displaying formatted resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,58 @@
-import { useState } from 'react'
+import { StrictMode, useState } from 'react'
+import root from './main'
 import General from './components/General'
 import Education from './components/Education'
 import Experience from './components/PracticalExperience'
 import './App.css'
+
+
+function Resume({ formData, educationItems, experienceItems}) {
+  
+  return (
+    <main>
+      <h2>{formData.fullName}</h2>
+      <p>{formData.email} | {formData.phone}</p>
+      <hr/>
+
+      <p>EDUCATION</p>
+      {educationItems.map(item => {
+          const school = `schoolName${item.id}`;
+          const title = `studyTitle${item.id}`;
+          const date = `graduationDate${item.id}`;
+      
+          console.log(formData[school])
+          return (
+            <section key={item.key}>
+              <p><strong>{formData[school]}</strong></p>
+              <p><em>{formData[title]}</em></p>
+              <p><em>{formData[date]}</em></p>
+            </section>
+          )
+      })}
+      <hr/>
+      
+      <p>EXPERIENCE</p>
+      {experienceItems.map(item => {
+        const company = `companyName${item.id}`;
+        const position = `positionTitle${item.id}`;
+        const description = `description${item.id}`;
+        const start = `startDate${item.id}`;
+        const end = `endDate${item.id}`;
+    
+        return (
+          <section key={item.key}>
+            <p><strong>{formData[company]}</strong></p>
+            <p><em>{formData[position]}</em></p>
+            <p><em>{`${formData[start]} - ${!end ? formData[end] : "Present"}`}</em></p>
+            <p>{formData[description]}</p>
+          </section>
+        )
+      })}
+
+    </main>
+  )
+}
+
 
 function App() {
   const [formData, setFormData] = useState();
@@ -16,7 +66,11 @@ function App() {
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log(formData);
+    root.render(
+      <StrictMode>
+        <Resume formData={formData} educationItems={educationItems} experienceItems={experienceItems}/>
+      </StrictMode>
+    )
   }
 
   return (
