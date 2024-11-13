09b98f8a17a171012a3bccf815556d5cb560ea04
dee797 <178827694+dee797@users.noreply.github.com>
2024-11-13 16:39:28 -0700

    Pass formData to CustomInputs, change input check box to CustomInput

--- src/components/PracticalExperience.jsx
+++ src/components/PracticalExperience.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import CustomInput from "./Input";
 
-function ExperienceFieldset({num, inputChange}) {
+function ExperienceFieldset({num, inputChange, formData}) {
     const [isChecked, setChecked] = useState(false);
 
     return (
@@ -10,42 +10,50 @@ function ExperienceFieldset({num, inputChange}) {
             <p>
                 <label>
                     Company Name:&nbsp;
-                    <CustomInput inputChange={inputChange} name={`companyName${num}`}/>
+                    <CustomInput inputChange={inputChange} name={`companyName${num}`} formData={formData}/>
                 </label>
             </p>
 
             <p>
                 <label>
                     Position Title:&nbsp;
-                    <CustomInput inputChange={inputChange} name={`positionTitle${num}`}/>
+                    <CustomInput inputChange={inputChange} name={`positionTitle${num}`} formData={formData}/>
                 </label>
             </p>
 
             <p>
                 <label>
                     Description:&nbsp;
-                    <CustomInput inputChange={inputChange} name={`description${num}`}/>
+                    <CustomInput inputChange={inputChange} name={`description${num}`} formData={formData}/>
                 </label>
             </p>
 
             <p>
                 <label>
                     Start Date:&nbsp;
-                    <CustomInput type="date" inputChange={inputChange} name={`startDate${num}`}/>
+                    <CustomInput type="date" inputChange={inputChange} name={`startDate${num}`} formData={formData}/>
                 </label>
             </p>
 
             <p>
                 <label>
                     End Date:&nbsp;
-                    <CustomInput type="date" disabled={isChecked} inputChange={inputChange} name={`endDate${num}`}/>
+                    <CustomInput type="date" disabled={isChecked} inputChange={inputChange} name={`endDate${num}`} formData={formData}/>
                 </label>
             </p>
 
             <p>
                 <label>
                     I currently work here&nbsp;
-                    <input type="checkbox" onClick={() => setChecked(isChecked => isChecked ? false : true)} />
+                    <CustomInput 
+                        type="checkbox" 
+                        inputChange={inputChange}
+                        name={`check${num}`}
+                        checked={isChecked}
+                        setCheck={setChecked}
+                        formData={formData}
+                        num={num}
+                        />
                 </label>
             </p>
 
@@ -54,14 +62,14 @@ function ExperienceFieldset({num, inputChange}) {
 }
 
 
-function Experience({ inputChange, experienceItems, setExperienceItems }) {
+function Experience({ inputChange, experienceItems, setExperienceItems, formData }) {
 
     return (
         <section>
             <h2>Practical Experience</h2>
             
             {experienceItems.map((item) => {
-                return <ExperienceFieldset key={item.key} num={item.id} inputChange={inputChange} /> 
+                return <ExperienceFieldset key={item.key} num={item.id} inputChange={inputChange} formData={formData}/> 
             })}
 
             <button type="button" onClick={() => setExperienceItems(oldItems => [...oldItems, {id: oldItems.length + 1, key: crypto.randomUUID()}])}>
